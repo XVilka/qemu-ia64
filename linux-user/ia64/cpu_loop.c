1bc8fe12f5c77289764fa87ccc7b658952436a13
Prashant Vaibhav <qemu@mercurysquad.com>
2011-07-11 02:37:27 +0530

    Finally, target ia64-linux-user builds. Now to actually get it to work....

--- linux-user/ia64/cpu_loop.c
+++ linux-user/ia64/cpu_loop.c
@@ -7,3 +7,24 @@ void target_cpu_copy_regs(CPUArchState *env, struct target_pt_regs *regs)
 {
         // Nothing special TODO
 }
+
+void cpu_loop(CPUIA64State *env)
+{
+    int trapnr;
+    // XXX needed later: target_siginfo_t info;
+
+    while (1) {
+        trapnr = cpu_ia64_exec (env);
+
+        switch (trapnr) {
+            // TODO: handle traps
+        default:
+            printf ("Unhandled trap: 0x%x\n", trapnr);
+            cpu_dump_state(env, stderr, fprintf, 0);
+            exit (1);
+        }
+        process_pending_signals (env);
+    }
+}
+
+
